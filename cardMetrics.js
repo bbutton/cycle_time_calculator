d51d8db1888572164640877074ae34eea4d70ac6
Brian Button <brian.button@ctl.io>
2016-04-02 02:40:23 -0500

    Added code lost due to not saving file in Atom

--- cardMetrics.js
+++ cardMetrics.js
@@ -1,17 +1,21 @@
 "use strict";
 
 class CardMetrics {
-	constructor(id, name, cycleTime, estimate) {
+	constructor(id, name, cycleTime, estimate, startingTimestamp, endingTimestamp) {
 		this.id = id;
 		this.name = name;
 		this.cycleTime = cycleTime;
 		this.estimate = estimate;
+		this.startingTimestamp = startingTimestamp;
+		this.endingTimestamp = endingTimestamp;
 	}
 
 	getName() { return this.name; }
 	getId() { return this.id; }
 	getCycleTime() { return this.cycleTime; }
 	getEstimate() { return this.estimate; }
+	getStartingTimestamp() { return startingTimestamp; }
+	getEndingTimestamp() { return endingTimestamp; }
 }
 
 module.exports = CardMetrics;
